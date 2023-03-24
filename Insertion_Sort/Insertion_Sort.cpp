2d53b685ed9d0f4473e188cf64c12613736cf481
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-03-24 14:44:09 +0700

    membuat prosedur while

--- Insertion_Sort/Insertion_Sort.cpp
+++ Insertion_Sort/Insertion_Sort.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input() {
+	while (true) {
+		cout << "masukan jumlah data pada array : ";
+		cin >> n;
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
+	}
+}
