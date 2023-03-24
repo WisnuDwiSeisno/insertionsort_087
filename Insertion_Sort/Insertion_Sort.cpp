6edbbad5998beef9b9502c73ebab0e68135385d3
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-03-24 15:04:30 +0700

    memperbaiki kesalah pada prosedur display

--- Insertion_Sort/Insertion_Sort.cpp
+++ Insertion_Sort/Insertion_Sort.cpp
@@ -53,8 +53,8 @@ void display() {
 	cout << "Element Array yang telah tersusun" << endl;
 	cout << "=================================" << endl;
 
-	for (int k = o; j < n; j++) {
-		cout << arr[j] << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << " ";
 	}
 }
 
